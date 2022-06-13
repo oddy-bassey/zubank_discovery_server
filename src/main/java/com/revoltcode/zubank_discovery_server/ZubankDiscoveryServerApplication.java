3afce7334c1cabab3d18c9c2d4775774463c46d3
oddy-bassey <oddybassey@gmail.com>
2022-06-13 12:03:18 +0100

    initial project setup

---
+++ src/main/java/com/revoltcode/zubank_discovery_server/ZubankDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.revoltcode.zubank_discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZubankDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZubankDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revoltcode/zubank_discovery_server/ZubankDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revoltcode.zubank_discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZubankDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
