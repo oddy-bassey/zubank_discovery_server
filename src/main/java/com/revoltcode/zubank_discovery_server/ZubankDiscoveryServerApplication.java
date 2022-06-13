2452d9bc537324a6dee7e32faaf7f1145ef6e004
oddy-bassey <oddybassey@gmail.com>
2022-06-13 12:09:12 +0100

    eureka server configured

--- src/main/java/com/revoltcode/zubank_discovery_server/ZubankDiscoveryServerApplication.java
+++ src/main/java/com/revoltcode/zubank_discovery_server/ZubankDiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package com.revoltcode.zubank_discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ZubankDiscoveryServerApplication {
 
 	public static void main(String[] args) {
